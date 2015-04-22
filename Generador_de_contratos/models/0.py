5b824834a590a508543be3319ebc9c004ff2f667
aaruizcarrera <aaruiz@outlook.es>
2015-04-21 20:23:16 -0500

    aplicacion
    Aplicacion base para el generador de contratos

---
+++ Generador_de_contratos/models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Generador de Contratos'
+settings.subtitle = ''
+settings.author = ''
+settings.author_email = 'aaruiz@udlanet.ec'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'mysql://root@localhost/gcontratos'
+settings.security_key = '36dfead6-30a3-4ed7-befc-49ea5a432acd'
+settings.email_server = 'localhost'
+settings.email_sender = ''
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
